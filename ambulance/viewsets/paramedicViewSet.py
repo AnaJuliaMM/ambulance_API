1f2a7c312866bc80f7fb4b2fd5367d6d1bc3e410
Ana Julia M Martins <ana_juh17@hotmail.com>
2023-09-16 14:56:08 -0300

    fix(paramedic): fixing the update and patch method

--- ambulance/viewsets/paramedicViewSet.py
+++ ambulance/viewsets/paramedicViewSet.py
@@ -28,24 +28,24 @@ class ParamedicViewSet(viewsets.ModelViewSet):
 
 
 
-    def get_object(self, data, key):
+    def get_ambulance(self, pk):
         """
-        A method that retrieves a value based on its key and found an object whose pk is equal to the value retrieved
-    
+        Retrieve an AmbulanceEntity object by its primary key.
+
         Args:
-            data (dict): receives the data  of a request
-            key (str): the key whose value contains the object pk
+            pk (int): The primary key of the AmbulanceEntity to retrieve.
 
         Raises:
-            Http404: an exception raises in case the key was not found
+            Http404: Raised when no AmbulanceEntity with the specified primary key is found.
 
         Returns:
-            AmbulanceEntity: returns an object whose pk is the one found in the data
+            AmbulanceEntity: An AmbulanceEntity object with the specified primary key.
         """
         try:
-            return  AmbulanceEntity.objects.get(pk = data.get(key))
-        except:
-            raise Http404
+            return AmbulanceEntity.objects.get(pk=pk)
+        except AmbulanceEntity.DoesNotExist:
+            raise Http404("Ambulance not found")
+
 
 
     def create(self, request, *args, **kwargs):
@@ -58,7 +58,7 @@ class ParamedicViewSet(viewsets.ModelViewSet):
             Dict: returns a structure with "message" key that prints a messsage of sucess and "data" key with the new object created
         """
         try:
-            ambulance = self.get_value(request.data, "ambulance")
+            ambulance = self.get_ambulance(request.data.get("ambulance"))
             serializer = ParamedicSerializer(data=request.data)
             serializer.is_valid(raise_exception=True)
             serializer.save(ambulance=ambulance)
@@ -67,8 +67,35 @@ class ParamedicViewSet(viewsets.ModelViewSet):
             return Response({'message': 'Erro de validação', 'errors': e.detail}, status=status.HTTP_400_BAD_REQUEST)
 
 
-        # Personalize a resposta de acordo com suas necessidades
-        return Response({'message': 'Objeto criado com sucesso', 'data': serializer.data})
+    def update(self, request, *args, **kwargs):
+        # Personalize aqui o código para o método PUT
+        try:
+            paramedic = self.get_object()
+            ambulance = self.get_ambulance(request.data.get("ambulance"))
+            serializer = ParamedicSerializer(paramedic, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save(ambulance=ambulance)
+            return Response({'message': 'Objeto atualizado com sucesso', 'data': serializer.data})
+        except ValidationError as e:
+            return Response({'message': 'Erro de validação', 'errors': e.detail}, status=status.HTTP_400_BAD_REQUEST)
+
+
+    
+    def partial_update(self, request, *args, **kwargs):
+        # Personalize aqui o código para o método PATCH
+        try:
+            paramedic = self.get_object()
+            ambulance = self.get_ambulance(request.data.get("ambulance"))
+            serializer = ParamedicSerializer(paramedic, data=request.data, partial=True)
+            serializer.is_valid(raise_exception=True)
+            serializer.save(ambulance = ambulance)
+            return Response({'message': 'Objeto atualizado parcialmente com sucesso', 'data': serializer.data})
+        except ValidationError as e:
+            return Response({'message': 'Erro de validação', 'errors': e.detail}, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
 
 
 
